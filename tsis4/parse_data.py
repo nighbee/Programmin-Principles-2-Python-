15394f2281a11537c429b16e5b01a1c6a901675c
Your Name <nightblue200416@gmail.com>
2023-02-19 00:49:39 +0600

    upd

--- tsis4/parse_data.py
+++ tsis4/parse_data.py
@@ -6,10 +6,28 @@ print(data)
 print("""Interface status
 ================================================================================""")
 print("""DN                                             Description          Speed                      MTU """)
-for i, k in data["imdata"][0]['l1PhysIf']["attributes"].items():
-    if i=='dn':
-        print(k,end="                          ")
-    if i=='speed':
-        print(k,end="                                         ")
-    if i=='mtu':
-        print(k, end="                   ")
+# for i, k in data["imdata"][0]['l1PhysIf']["attributes"].items():
+#     if i=='dn':
+#         print(k,end="                          ")
+#     if i=='speed':
+#         print(k,end="                                         ")
+#     if i=='mtu':
+#         print(k, end="                   ")
+
+# for k in data["imdata"][i]['l1PhysIf']:
+#         for j in data["imdata"][i]['l1PhysIf']["attributes"].items():
+#             if k=='dn':
+#                 print(j, end="                          ")
+#             if k== 'speed':
+#                 print(j, end="                                         ")
+#             if k == 'mtu':
+#                 print(j, end="                   ")
+lenght=0
+for i, k in data["imdata"][lenght]['l1PhysIf']["attributes"].items:
+    if i == 'dn':
+                print(k, end="                          ")
+    if i== 'speed':
+                print(k, end="                                         ")
+    if i == 'mtu':
+                print(k, end="                   ")
+    lenght+=1
