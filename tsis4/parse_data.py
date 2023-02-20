656729f1e4a4d768170a069afdca86fda8458976
Your Name <nightblue200416@gmail.com>
2023-02-20 21:46:23 +0600

    shit

--- tsis4/parse_data.py
+++ tsis4/parse_data.py
@@ -1,33 +1,19 @@
 import json
 
-with open("data.json", "r") as read_file:
-    data=json.load(read_file)
-print(data)
-print("""Interface status
-================================================================================""")
-print("""DN                                             Description          Speed                      MTU """)
-# for i, k in data["imdata"][0]['l1PhysIf']["attributes"].items():
-#     if i=='dn':
-#         print(k,end="                          ")
-#     if i=='speed':
-#         print(k,end="                                         ")
-#     if i=='mtu':
-#         print(k, end="                   ")
+jsondata = open('data.json').read()
+aasas = json.loads(jsondata)
+print("=======================================================================================" "\n"
+      "DN                                                 Description           Speed    MTU" "\n"
+      "-------------------------------------------------- --------------------  ------  ------"
+      )
+imdata = aasas["imdata"]
+for i in imdata:
+    dn = i["l1PhysIf"]["attributes"]["dn"]
+    descr = i["l1PhysIf"]["attributes"]["descr"]
+    speed = i["l1PhysIf"]["attributes"]["speed"]
+    mtu = i["l1PhysIf"]["attributes"]["mtu"]
+    print("{0} {1} {2} {3}".format(dn, descr, speed, mtu))
+    if dn == "topology/pod-1/node-201/sys/phys-[eth1/35]":
+        break
 
-# for k in data["imdata"][i]['l1PhysIf']:
-#         for j in data["imdata"][i]['l1PhysIf']["attributes"].items():
-#             if k=='dn':
-#                 print(j, end="                          ")
-#             if k== 'speed':
-#                 print(j, end="                                         ")
-#             if k == 'mtu':
-#                 print(j, end="                   ")
-lenght=0
-for i, k in data["imdata"][lenght]['l1PhysIf']["attributes"].items:
-    if i == 'dn':
-                print(k, end="                          ")
-    if i== 'speed':
-                print(k, end="                                         ")
-    if i == 'mtu':
-                print(k, end="                   ")
-    lenght+=1
+
