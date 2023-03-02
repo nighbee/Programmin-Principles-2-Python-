7a65289dbb44a99f363471abe3ba3c797f06df66
Your Name <nightblue200416@gmail.com>
2023-03-02 20:30:48 +0600

    upd

--- tsis6/dir_shit.py
+++ tsis6/dir_shit.py
@@ -1,10 +1,15 @@
-import os
+import os,  string
 #1 listing directories and files in path
 path=input('Enter your shitty path: ')
 os.chdir(path)
 print(os.listdir(path))
 #2
-
+if os.path.exists(path):
+    filename=os.path.basename(path)
+    directory=os.path.diname(path)
+    print(filename, directory)
+else:
+    print('Not found')
 
 # 3 check if path exists or not
 print(os.path.exists(path))
@@ -16,4 +21,33 @@ with open(file_name, 'r') as f:
         count+=1
     print(count)
     f.close()
-# 5
+#5 list to file
+items=['mango', 'orange', 'apple', 'lemon']
+with open(file_name) as f:
+    for it in items:
+        f.write(it+"\n")
+    f.close()
+# 6 generate 26 files which names A to Z.txt
+if not os.path.exists("letters"):
+   os.makedirs("letters")
+for letter in string.ascii_uppercase:
+   with open(letter + ".txt", "w") as f:
+       f.writelines(letter)
+#7 copy and paste to another doc
+second_file=input('Second file which u gonna paste info from first one: ')
+with open(file_name, 'r') as f:
+    with open(second_file, 'w') as f1:
+        for line in f:
+            f1.write(line)
+        f1.close()
+    f.close()
+
+#8
+if os.path.exists(path):
+    f=input('enter file name which wanna to delete: ')
+    os.chdir(path)
+    os.remove(f)
+else:
+    print('Path not found! resubmit')
+
+
